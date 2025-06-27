070be8ec608148781b56278f1817730e0b9f90ed
JMit-dev <darkkid8192004@gmail.com>
2025-06-27 14:39:30 -0400

    fix: wrong function signature

--- engine/include/Engine/Renderer/VertexBuffer.h
+++ engine/include/Engine/Renderer/VertexBuffer.h
@@ -1,13 +1,15 @@
 #ifndef VERTEX_BUFFER_H
 #define VERTEX_BUFFER_H
 
+#include <glad/glad.h>
+
 namespace Engine::Renderer {
 class VertexBuffer {
 public:
   VertexBuffer(const void *data, unsigned size) {
     glGenBuffers(1, &m_id);
     bind();
-    glGenBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
   }
   ~VertexBuffer() { glDeleteBuffers(1, &m_id); }
   void bind() const { glBindBuffer(GL_ARRAY_BUFFER, m_id); }
